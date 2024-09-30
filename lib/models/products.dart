4be0252753038228d1844c6142191a7af6f8e069
tanjilalom <tanjil15-3079@diu.edu.bd>
2024-09-30 18:21:38 +0600

    UI update

---
+++ lib/models/products.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final String title;
+  final String description;
+  final String image;
+  final double price;
+  final List<Color> colors;
+  final String category;
+  final double rate;
+
+  Product({
+    required this.title,
+    required this.description,
+    required this.image,
+    required this.price,
+    required this.colors,
+    required this.category,
+    required this.rate,
+  });
+}
+
+final List<Product> products = [
+  Product(
+    title: 'Wireless Headphone',
+    description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    image: 'assets/images/wireless.png',
+    price: 210,
+    colors: [Colors.black, Colors.blue, Colors.orange],
+    category: 'Woman Fashion',
+    rate: 4.8,
+  ),
+  Product(
+    title: 'Woman Sweater',
+    description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    image: 'assets/images/sweet.png',
+    price: 120,
+    colors: [Colors.brown.shade200, Colors.red, Colors.pink],
+    category: 'Woman Fashion',
+    rate: 4.8,
+  ),
+  Product(
+    title: 'Smart Watch',
+    description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    image: 'assets/images/watch.png',
+    price: 55,
+    colors: [
+      Colors.black,
+    ],
+    category: 'Watch',
+    rate: 4.8,
+  ),
+];
---
+++ lib/widgets/products_card.dart
@@ -0,0 +1,98 @@
+import 'package:ecommerce_mobile_app/constants.dart';
+import 'package:ecommerce_mobile_app/models/products.dart';
+import 'package:ecommerce_mobile_app/screens/product_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+
+class ProductsCard extends StatelessWidget {
+  const ProductsCard({super.key, required this.product});
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(CupertinoPageRoute(
+          builder: (context) => ProductScreen(product: product),
+        ));
+      },
+      child: Stack(
+        children: [
+          Container(
+            height: 250,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: kcontantColor,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                Image.asset(
+                  product.image,
+                  width: 120,
+                  height: 120,
+                ),
+                Text(
+                  product.title,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      "\$${product.price}",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Row(
+                      children: List.generate(
+                        product.colors.length,
+                        (cindex) => Container(
+                          height: 15,
+                          width: 15,
+                          margin: const EdgeInsets.only(right: 3),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: product.colors[cindex],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 30,
+                height: 30,
+                decoration: BoxDecoration(
+                  color: kprimaryColor,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(10),
+                  ),
+                ),
+                child: Icon(
+                  Ionicons.heart_outline,
+                  color: Colors.white,
+                  size: 18,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
